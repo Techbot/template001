5e6917bb488577beac54ac36df4ce37ea98fe6fa
left23 <lcdemc@gmail.com>
2013-11-05 21:12:25 +0000

    edited link to template downloaded to point to GitHub

--- view.php
+++ view.php
@@ -1,36 +1,40 @@
 <!DOCTYE html><!-- this is HTML5 -->
 <html>
-<head>
-<title>Browser Bar Title</title>
-<link rel=stylesheet href=styles.css>
-<script src=script.js></script>
-<script src=canvas_stuff.js></script>
 
-</head>
-<body>
+	<head>
+		<title>Browser Bar Title</title>
+		<link rel=stylesheet href=styles.css>
+		<script src=script.js></script>
+		<script src=canvas_stuff.js></script>
 
-<div id="container">
-<h3 id="title">Battle</h3>
+	</head>
+	
+	<body>
 
-<p>
-<a href="http://coderdojogalway.com/index.php?option=com_kunena&view=topic&catid=3&id=211&Itemid=279">
-Download the Template</a>
-</p>
+		<div id="container">
 
-<canvas id="myCanvas" width="568" height="300">
-</canvas>
+			<h3 id="title">Battle</h3>
 
-<div id ="float_left">
--x-
-</div>
+			<p>
+			<a href="https://github.com/Techbot/template001">
+			Download the Template from GitHub</a>
+			</p>
 
-<div id ="float_right">
--x-
-</div>
+			<canvas id="myCanvas" width="568" height="300">
+			</canvas>
 
-</div><!-- end container -->
+			<div id ="float_left">
+			-x-
+			</div>
+
+			<div id ="float_right">
+			-x-
+			</div>
+
+		</div><!-- end container -->
 
 
-</body>
+	</body>
+	
 </html>
 
