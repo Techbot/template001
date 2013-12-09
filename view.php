3f7998c120e2851ed8ed4ce793c86a5eee03e37e
techbot <djtechbot@gmail.com>
2013-12-09 13:18:00 +0000

    edit notes on md file

--- view.php
+++ view.php
@@ -1,36 +1,36 @@
 <!DOCTYE html><!-- this is HTML5 -->
 <html>
-<head>
-<title>Browser Bar Title</title>
-<link rel=stylesheet href=styles.css>
-<script src=script.js></script>
-<script src=canvas_stuff.js></script>
+    <head>
+        <title>Template001</title>
+        <link rel=stylesheet href=styles.css>
+        <script src=script.js></script>
+        <script src=canvas_stuff.js></script>
 
-</head>
-<body>
+    </head>
+    <body>
 
-<div id="container">
-<h3 id="title">Battle</h3>
+        <div id="container">
+            <h3 id="title">Battle</h3>
 
-<p>
-<a href="http://coderdojogalway.com/index.php?option=com_kunena&view=topic&catid=3&id=211&Itemid=279">
-Download the Template</a>
-</p>
+            <p>
+                <a href="http://coderdojogalway.com/index.php?option=com_kunena&view=topic&catid=3&id=211&Itemid=279">
+                Download the Template</a>
+            </p>
 
-<canvas id="myCanvas" width="568" height="300">
-</canvas>
+            <canvas id="myCanvas" width="568" height="300">
+            </canvas>
 
-<div id ="float_left">
--x-
-</div>
+            <div id ="float_left">
+            -x-
+            </div>
 
-<div id ="float_right">
--x-
-</div>
+            <div id ="float_right">
+            -x-
+            </div>
 
-</div><!-- end container -->
+        </div><!-- end container -->
 
 
-</body>
+    </body>
 </html>
 
